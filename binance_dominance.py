f9f4ee241ef3f2ffdc9ea18f1a29c8f3512e66ed
ling.w.hai <91795943+h11329763@users.noreply.github.com>
2022-11-20 23:21:21 +0800

    Add files via upload
    Use it if you like this.
    Plz share your experience with me.

---
+++ binance_dominance.py
@@ -0,0 +1,183 @@
+###############################################################################
+#Binance Part on ERC20
+Binance_Hot = {}
+
+Binance_Hot[1] = '0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE'
+Binance_Hot[2] = '0xD551234Ae421e3BCBA99A0Da6d736074f22192FF'
+Binance_Hot[3] = '0x564286362092D8e7936f0549571a803B203aAceD'
+Binance_Hot[4] = '0x0681d8Db095565FE8A346fA0277bFfdE9C0eDBBF'
+Binance_Hot[5] = '0xfE9e8709d3215310075d67E3ed32A380CCf451C8'
+Binance_Hot[6] = '0x4E9ce36E442e55EcD9025B9a6E0D88485d628A67'
+Binance_Hot[7] = '0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8'
+Binance_Hot[8] = '0xf977814e90da44bfa03b6295a0616a897441acec'
+Binance_Hot[9] = '0x001866Ae5B3de6cAa5a51543FD9fB64f524F5478'
+Binance_Hot[10] = '0x85b931A32a0725Be14285B66f1a22178c672d69B'
+Binance_Hot[11] = '0x708396f17127c42383E3b9014072679b2F60B82f'
+Binance_Hot[12] = '0xE0F0CfDe7Ee664943906f17F7f14342E76A5CeC7'
+Binance_Hot[13] = '0x8F22F2063D253846B53609231eD80FA571Bc0C8F'
+Binance_Hot[14] = '0x28c6c06298d514db089934071355e5743bf21d60'
+Binance_Hot[15] = '0x21a31ee1afc51d94c2efccaa2092ad1028285549'
+Binance_Hot[16] = '0xdfd5293d8e347dfe59e90efd55b2956a1343963d'
+Binance_Hot[17] = '0x56Eddb7aa87536c09CCc2793473599fD21A8b17F'
+Binance_Hot[18] = '0x9696f59E4d72E237BE84fFD425DCaD154Bf96976'
+Binance_Hot[19] = '0x4D9fF50EF4dA947364BB9650892B2554e7BE5E2B'
+Binance_Hot[20] = '0x4976A4A02f38326660D17bf34b431dC6e2eb2327'
+Binance_Hot[21] = '0xd88B55467f58af508dBfDC597E8Ebd2Ad2De49b3'
+Binance_Hot[22] = '0x7DFe9A368B6Cf0C0309b763bb8d16da326e8F46e'
+Binance_Hot[23] = '0x345D8e3A1F62eE6B1D483890976fD66168e390F2'
+Binance_Hot[24] = '0xC3C8E0A39769e2308869f7461364ca48155D1d9E'
+Binance_Hot[25] = '0x2E581a5aE722207Aa59aCD3939771E7c7052DD3d'
+Binance_Hot[26] = '0x44592b81c05b4c35Efb8424eB9D62538b949eBbF'
+Binance_Hot[27] = '0xd5fd1BC99d5801278345e9D29bE2225D06c26E93'
+Binance_Hot[28] = ''
+Binance_Hot[29] = '0x06a0048079ec6571Cd1b537418869CDE6191d42D'
+Binance_Hot[30] = '0x892e9e24AeA3f27f4C6E9360e312Cce93cc98Ebe'
+Binance_Hot[31] = '0x00799bbc833D5B168F0410312d2a8fD9e0e3079c'
+Binance_Hot[32] = '0x141FeF8cd8397a390AFe94846c8bD6F4ab981c48'
+Binance_Hot[33] = '0x50d669F43b484166680Ecc3670E4766cdb0945CE'
+Binance_Hot[34] = '0x2f7e209e0F5F645c7612D7610193Fe268F118b28'
+Binance_Hot[35] = '0xd9D93951896B4eF97D251334EF2A0e39F6F6D7d7'
+Binance_Hot[36] = '0x5a52e96bacdabb82fd05763e25335261b270efcb'
+Binance_Hot[37] = '0xa344c7aDA83113B3B56941F6e85bf2Eb425949f3'
+##############################################################################
+###############################################################################
+#Binance Part on BEP20
+Binance_Hot2 = {}
+
+Binance_Hot2[1] = '0x631fc1ea2270e98fbd9d92658ece0f5a269aa161'
+Binance_Hot2[2] = '0xb1256d6b31e4ae87da1d56e5890c66be7f1c038e'
+Binance_Hot2[3] = '0x17b692ae403a8ff3a3b2ed7676cf194310dde9af'
+Binance_Hot2[4] = '0x8ff804cc2143451f454779a40de386f913dcff20'
+Binance_Hot2[5] = '0xad9ffffd4573b642959d3b854027735579555cbc'
+Binance_Hot2[6] = '0x8894e0a0c962cb723c1976a4421c95949be2d4e3'
+Binance_Hot2[7] = '0xe2fc31f816a9b94326492132018c3aecc4a93ae1'
+Binance_Hot2[8] = '0x3c783c21a0383057d128bae431894a5c19f9cf06'
+Binance_Hot2[9] = '0xdccf3b77da55107280bd850ea519df3705d1a75a'
+Binance_Hot2[10] = '0xeb2d2f1b8c558a40207669291fda468e50c8a0bb'
+Binance_Hot2[11] = '0x161ba15a5f335c9f06bb5bbb0a9ce14076fbb645'
+Binance_Hot2[12] = '0x515b72ed8a97f42c568d6a143232775018f133c8'
+Binance_Hot2[13] = '0xbd612a3f30dca67bf60a39fd0d35e39b7ab80774'
+Binance_Hot2[14] = '0x7a8a34db9acd10c3b6277473b192fe47192569ca'
+Binance_Hot2[15] = ''
+Binance_Hot2[16] = '0xa180fe01b906a1be37be6c534a3300785b20d947'
+Binance_Hot2[17] = '0x29bdfbf7d27462a2d115748ace2bd71a2646946c'
+Binance_Hot2[18] = '0x73f5ebe90f27b46ea12e5795d16c4b408b19cc6f'
+Binance_Hot2[19] = '0x1fbe2acee135d991592f167ac371f3dd893a508b'
+Binance_Hot2[20] = '0xf977814e90da44bfa03b6295a0616a897441acec'
+Binance_Hot2[21] = '0xbe0eb53f46cd790cd13851d5eff43d12404d33e8'
+Binance_Hot2[22] = '0x5a52e96bacdabb82fd05763e25335261b270efcb'
+##############################################################################
+def coin_balance_eth(coin_contract, address):
+    
+    if len(address) < 1:
+        return(0)
+    else:
+        return(np.float(eth.get_acc_balance_by_token_and_contract_address(coin_contract, address))/1E18)
+
+def coin_total_eth(coin_contract):
+    return(np.float(eth.get_total_supply_by_contract_address(coin_contract))/1E18)
+
+def coin_total_bsc(coin_contract_bsc,bsc_API):
+    if len(coin_contract_bsc) < 1:
+        return(0)
+    else:
+        import requests
+
+        urllink1 = 'https://api.bscscan.com/api?module=stats'
+        urllink2 = '&action=tokensupply&contractaddress=%s&apikey=%s'%(coin_contract_bsc,bsc_API)
+        
+        r = requests.get(urllink1+urllink2, stream=True)
+        
+        for line in r.iter_lines():
+            
+            return(np.float(line[39:-2])/1E18)
+        
+def coin_balance_bsc(coin_contract, address,bsc_API):    
+    if len(address) < 1:
+        return(0)
+    else:
+        import requests
+        import struct
+
+        urllink1 = 'https://api.bscscan.com/api?module=account'
+        urllink2 = '&action=tokenbalance&contractaddress=%s&address=%s&tag=latest&apikey=%s'%(coin_contract,address,bsc_API)
+        
+        r = requests.get(urllink1+urllink2, stream=True)
+        
+        for line in r.iter_lines():
+            return(np.float(line[39:-2])/1E18)
+
+def coin_name_bsc(coin_contract_bsc,bsc_API):
+    import requests
+    import json
+    
+    urllink1 = 'https://api.bscscan.com/api?module=contract'
+    urllink2 = '&action=getsourcecode&address=%s&apikey=%s'%(coin_contract_bsc,bsc_API)
+    
+    r = requests.get(urllink1+urllink2, stream=True)
+    
+    for line in r.iter_lines():
+        Z = line
+    
+    my_dict = json.loads(Z.decode('utf-8'))
+    
+    return(my_dict['result'][0]['ContractName'])
+###############################################################################
+
+from etherscan import Etherscan
+import numpy as np
+
+bsc_API = '11FGIB5HM2BXP262N2FAUHZ17XBB2QY3Z8'
+eth_API = '67PH3U6643C3GZYBHZGVR455PAHF3E4XB7'
+
+coin_contract_eth = ''
+coin_contract_bsc = '0x0409633A72D846fc5BBe2f98D88564D35987904D'
+
+if len(coin_contract_eth) < 1:
+   XD = 1
+else:
+    eth = Etherscan(eth_API)
+    
+    Token_Name = eth.get_contract_source_code(coin_contract_eth)[0]['ContractName']
+    coin_total = coin_total_eth(coin_contract_eth)
+
+
+if len(coin_contract_bsc) < 1:
+   XD = 1
+else:
+    Token_Name = coin_name_bsc(coin_contract_bsc,bsc_API)
+    coin_total = coin_total_bsc(coin_contract_bsc,bsc_API)
+
+binance_eth_balance = 0
+if len(coin_contract_eth) < 1:
+   XD = 1
+else:
+    for i in range (1, 38):
+         binance_eth_balance = binance_eth_balance + coin_balance_eth(coin_contract_eth, Binance_Hot[i])
+         #print('Hot Wallet Count on ERC20', i)
+
+binance_bsc_balance = 0
+if len(coin_contract_bsc) < 1:
+   XD = 1
+else:
+    for i in range (1, 23):
+         binance_bsc_balance = binance_bsc_balance + np.float(coin_balance_bsc(coin_contract_bsc, Binance_Hot2[i],bsc_API))
+         #print('Hot Wallet Count on BEP20', i)
+    
+binance_eth_percent =  ((binance_eth_balance)/coin_total) * 100 
+binance_bsc_percent =  ((binance_bsc_balance)/coin_total) * 100
+    
+binance_percentage = binance_eth_percent + binance_bsc_percent
+print('###################################################################')
+print('Token         Name:',Token_Name)
+print('Token Contract ETH:', coin_contract_eth)
+print('Token Contract BEP:', coin_contract_bsc)
+print('Token Total Amount:', coin_total)
+print('Binance ETH Amount:',binance_eth_balance)
+print('Binance BEP Amount:',binance_bsc_balance)
+print('Binance Total Amnt:',binance_eth_balance + binance_bsc_balance)
+print('Binance  ETH  Prct:','%5.2f'%binance_eth_percent,'%')
+print('Binance  BEP  Prct:','%5.2f'%binance_bsc_percent,'%')
+print('Binance Total Prct:','%5.2f'%binance_percentage,'%')
+print('###################################################################')
+#'''
